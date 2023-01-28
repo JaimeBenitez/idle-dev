8ade881c8734d499b1464fd4f4083286445c261a
jaime <jbenaci736@g.educaaand.es>
2023-01-28 20:09:51 +0100

    Primer commit con pantalla inicial

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="contenedor-logo-inicio">
+    <img src="../assets/idledev-logo.png" alt="logo" class="logo">
+  </div>
+  <nav class="button-nav">
+    <a href="registro.html" class="button">Registrarse</a>
+    <a href="login" class="button">Login</a>
+    <a href="contacto" class="button">Contacto</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
