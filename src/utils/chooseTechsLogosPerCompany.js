bc76b9be9ff8505af961376ab6ea86660747cde7
JaimeBenitez <jbenaci50@gmail.com>
2023-05-09 18:14:34 +0200

    Añadidos resto de logos a la pantalla de detalle de empresa. Parseador y desbloqueo de empresas arreglado

--- src/utils/chooseTechsLogosPerCompany.js
+++ src/utils/chooseTechsLogosPerCompany.js
@@ -4,6 +4,14 @@ import JS from '@/assets/js.svg'
 import Node from '@/assets/nodejs.svg'
 import Java from '@/assets/java.svg'
 import PHP from '@/assets/php.svg'
+import Python from '@/assets/python.svg'
+import Kotlin from '@/assets/kotlin.svg'
+import OS from '@/assets/os.svg'
+import Despliegue from '@/assets/production.svg'
+import RN  from '@/assets/react-native.svg'
+import Swift from '@/assets/swift.svg'
+import Unity from '@/assets/unity.svg'
+import Unreal from '@/assets/unreal-engine.svg'
 
 export default function chooseTechsLogosPerCompany(companyId){
     switch(companyId){
@@ -14,19 +22,36 @@ export default function chooseTechsLogosPerCompany(companyId){
           { "src": JS, "alt": "JS" }
           ]
       case 2: 
-          return[
+        return[
           { "src": Node,"alt": "Node" },
           { "src": Java,"alt": "Java" },
           { "src": PHP, "alt": "PHP" }
           ]
       case 3:
-      return[
-            { "src": HTML, "alt": "HTML" },
-            { "src": CSS, "alt": "CSS" },
-            { "src": JS, "alt": "JS" },
-            { "src": Node,"alt": "Node" },
-            { "src": Java,"alt": "Java" },
-            { "src": PHP, "alt": "PHP" }
-          ] 
+        return[
+              { "src": HTML, "alt": "HTML" },
+              { "src": CSS, "alt": "CSS" },
+              { "src": JS, "alt": "JS" },
+              { "src": Node,"alt": "Node" },
+              { "src": Java,"alt": "Java" },
+              { "src": PHP, "alt": "PHP" },
+              { "src": Python, "alt": "Python" },
+            ] 
+      case 4:
+        return[
+          { "src": Kotlin, "alt": "Kotlin" },
+          { "src": Swift, "alt": "Swift" },
+          { "src": RN, "alt": "React Native" },
+        ] 
+      case 5:
+        return[
+          { "src": Despliegue, "alt": "Despliegue" },
+          { "src": OS, "alt": "OS" },
+        ] 
+      case 6:
+        return[
+          { "src": Unity, "alt": "Unity" },
+          { "src": Unreal, "alt": "Unreal Engine" },
+        ] 
     }
   }
--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -10,7 +10,29 @@ export default function formatNumber(numberToFormat,units,avoidUnitDecimals = fa
     }
     else if(numberToFormat/1e+12 >= 1 && numberToFormat/1e+12 < 1000){
       numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "T"  
-    }else if(!avoidUnitDecimals){      
+    }
+    else if(numberToFormat/1e+15 >= 1 && numberToFormat/1e+15 < 1000){
+      numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "Q"  
+    }
+    else if(numberToFormat/1e+18 >= 1 && numberToFormat/1e+18 < 1000){
+      numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "Qi"  
+    }
+    else if(numberToFormat/1e+21 >= 1 && numberToFormat/1e+21 < 1000){
+      numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "Sx"  
+    }
+    else if(numberToFormat/1e+24 >= 1 && numberToFormat/1e+24 < 1000){
+      numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "Sp"  
+    }
+    else if(numberToFormat/1e+27 >= 1 && numberToFormat/1e+27 < 1000){
+      numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "Oc"  
+    }
+    else if(numberToFormat/1e+30 >= 1 && numberToFormat/1e+30 < 1000){
+      numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "No"  
+    }
+    else if(numberToFormat/1e+33 >= 1 && numberToFormat/1e+33 < 1000){
+      numberToFormat = (numberToFormat / 1e+12).toFixed(2) + "Dc"  
+    }
+    else if(!avoidUnitDecimals){      
       numberToFormat = numberToFormat.toFixed(2)
     }
     return numberToFormat + units
